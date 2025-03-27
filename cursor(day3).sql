ad6c8da8a6392a5c49f18943004c88d4dd173881
mahakbabel12 <140581938+mahakbabel12@users.noreply.github.com>
2025-03-27 06:48:19 +0530

    Add files via upload

---
+++ cursor(day3).sql
@@ -0,0 +1,35 @@
+SET SERVEROUTPUT ON;
+
+declare
+    v_count int:=0;
+    
+begin 
+    if SQL%FOUND THEN
+        dbms_output.put_line('cursor open');
+    select count(*) into v_count from test where id>10;
+   end if;
+   
+end;
+/
+declare
+    v_count int:=0;
+    
+begin 
+    if SQL%FOUND THEN
+        dbms_output.put_line('cursor open 1'|| ' ' || sql%rowcount);
+    end if;
+    update test set id =50;
+    if SQL%FOUND THEN
+        dbms_output.put_line('cursor open 2' || ' ' || sql%rowcount);
+   end if;
+   
+end;
+/
+
+
+insert into test values(12);
+insert into test values(13);
+insert into test values(14);
+
+select * from test;
+
